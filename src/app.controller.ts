6e862bb268da3976df68ef20d81885e49dd6e375
roydhriti <roydhriti240@gmail.com>
2026-09-18 18:40:55 +0530

    push for ec2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,13 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('health')
+  healthCheck() {
+    return {
+      status: 'ok',
+      message: 'Application is healthy',
+      timestamp: new Date().toISOString(),
+    };
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // await app.listen(process.env.PORT ?? 3000);
+
   const port = process.env.PORT || 3000;
 
-  await app.listen(port, '0.0.0.0');
+  await app.listen(8000, '0.0.0.0');
   console.log(`Application running on port ${port}`);
 }
 bootstrap();
+
+// "build": "nest build",
+
+// "build": "nest build && docker build -t aws-try:latest .",
+// await app.listen(process.env.PORT ?? 3000);
