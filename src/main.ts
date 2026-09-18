4e62d4c8c15a74fcb5f3c6eef00cfff60aa91161
roydhriti <roydhriti240@gmail.com>
2026-09-18 19:15:32 +0530

    push

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
-  await app.listen(8000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
   console.log(`Application running on port ${port}`);
 }
 bootstrap();
-
-// "build": "nest build",
-
-// "build": "nest build && docker build -t aws-try:latest .",
-// await app.listen(process.env.PORT ?? 3000);
